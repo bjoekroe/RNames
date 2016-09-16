86ba1bcddbd4343a12422ed3b374b8b9f4b67965
bjoekroe <bjorn.kroger@helsinki.fi>
2016-09-16 16:28:15 +0300

    Add files via upload

---
+++ 3_pb_rnames_match.R
@@ -0,0 +1,255 @@
+#########################################################################################################################################
+###### This file creates a list of time binned Paleobiology Database occurrences based on opinions tables in Rnames ################################
+###### currently three options are available for time binning: Global Stages, Bergström et al. (2009), and Webby et al. (2004) TS, #####
+###### the result will be written into a csv file ######################################################################################
+########################################################################################################################################
+###### before running specify work directory, time resolution, and data sources ######
+##### !run needs several hours! ###
+
+library(DBI)
+library(RMySQL)
+library(gdata)
+source("/Users/bkroger/Documents/r/rnames/rname_finals/rname_functions.r");
+setwd("/Users/bkroger/Documents/r/rnames/rname_finals/");
+
+
+###########################
+#### select time resolution 
+selection <- "b" ## b - bergström stage slices, c - chronostrat. stages, w - webby time slices
+
+
+############################
+### optional: download from PBDB Data from original sources or use previously downloaded files
+
+http<-paste("http://paleobiodb.org/data1.2/occs/list.txt?interval=Ordovician&show=stratext,geo,loc")
+http2<-paste("http://paleobiodb.org/data1.2/colls/list.txt?interval=Ordovician&show=ref,loc,stratext,paleoloc")
+
+ordo_tot <-read.table(http, sep=',', header=T)
+coll_tot <- read.table(http2, sep=',', header=T)
+coll_tots <- coll_tot[,c(1,36)]
+
+write.csv(ordo_tot, file="pbdb_ds.csv")
+write.csv(coll_tots, file="coll_tots.csv")
+
+### This is a shortcut if files are already downloaded from PBDB
+#ordo_tot <-read.table(file="pbdb_ds.csv", sep=',', header=T)
+#coll_tots <-read.table(file="coll_tots.csv", sep=',', header=T)
+
+############################
+### optional: download opinion tables from Rnames DB or use previously downloaded files
+
+# rn.con <- dbConnect(m.drv, dbname = db.db, username = db.user, password = db.pw, host = db.url)
+# dbGetQuery(rn.con," SET NAMES utf8")
+# opb <- dbGetQuery(rn.con,"select * from opinions_bergstrom")
+# opb <- cbind(opb[,1], opb)
+# opc <- dbGetQuery(rn.con,"select * from opinions_chrono")
+# opc <- cbind(opc[,1], opc)
+# opw <- dbGetQuery(rn.con,"select * from opinions_webby")
+# opw <- cbind(opw[,1], opw)
+# dbDisconnect(rn.con)
+
+opb <-read.table(file="B_TS.csv", sep=',', header=T) # load Bergstroem ts data
+opc <-read.table(file="C_TS.csv", sep=',', header=T) # load stage slice data
+opw <-read.table(file="W_TS.csv", sep=',', header=T) # load Webby slice data
+
+bts <- cbind(Bergst_ts,row.names(Bergst_ts))
+wts <- cbind(Webby_ts,row.names(Webby_ts))
+cts <- cbind(Chrono_ts,row.names(Chrono_ts))
+
+#########################
+########## additional preparation of input data
+
+opc[(nrow(opc)+1),] <- c("","","","0", "Katian", "Katian", "Katian", "1","0")
+levels(opc[,5])[nrow(opc)] <- "Katian"
+opc[(nrow(opc)),5] <- "Katian"
+ordo_tot <- subset(ordo_tot, ordo_tot$identified_rank == "genus" | ordo_tot$identified_rank == "species" ||  ordo_tot$identified_no>0)
+ordo_tot <- subset(ordo_tot, ordo_tot$identified_no>0)
+
+os <- as.data.frame(ordo_tot[,c("early_interval", "late_interval", "stratgroup", "formation", "member", "zone", "localbed", "regionalbed", "stratcomments")])
+colnames(os) <- c("early_interval", "late_interval", "stratgroup", "formation", "member", "zone", "localbed", "regionalbed", "stratcomments")
+
+osu <- unique(os)
+
+os.c <- os
+os.c <- as.data.frame(os.c)
+os.c$fmm <- apply(format(os.c[,4:5]), 1, paste, collapse=" ")
+os.c$flb <- apply(format(os.c[,c(4,7)]), 1, paste, collapse=" ")
+os.c$id  <- 1:nrow(os.c)
+
+#############################
+#### load manually matched PBDB names from Rnames (= names in Rnames table "pbdb_names" which are linked to structured_names in Rnames
+
+rn.con <- dbConnect(m.drv, dbname = db.db, username = db.user, password = db.pw, host = db.url)
+dbGetQuery(rn.con," SET NAMES utf8")
+pb.m <- dbGetQuery(rn.con,"select * from pbdb_name where structured_name_id is not null")
+dbDisconnect(rn.con)
+pb.m <- pb.m[,4:6]
+pb.u <- pb.m[!(duplicated(pb.m[c("pbdb_name","structured_name_id")]) | duplicated(pb.m[c("pbdb_name","structured_name_id")])), ]
+
+rn.con <- dbConnect(m.drv, dbname = db.db, username = db.user, password = db.pw, host = db.url)
+dbGetQuery(rn.con," SET NAMES utf8")
+q.r <- dbGetQuery(rn.con,"select * from qualifier")
+dbDisconnect(rn.con)
+
+rn.con <- dbConnect(m.drv, dbname = db.db, username = db.user, password = db.pw, host = db.url)
+dbGetQuery(rn.con," SET NAMES utf8")
+n.r <- dbGetQuery(rn.con,"select * from name")
+dbDisconnect(rn.con)
+
+rn.con <- dbConnect(m.drv, dbname = db.db, username = db.user, password = db.pw, host = db.url)
+dbGetQuery(rn.con," SET NAMES utf8")
+sn.r <- dbGetQuery(rn.con,"select * from structured_name")
+dbDisconnect(rn.con)
+
+########################
+####### merge PBDB  data with Rnames: os.c = prepared PBDB occurrences; opx = time frames, either: opb= bergs, opw=webby, opc=chronostages
+####### x_ts: ts scheme from rname_functions.R either Bergstr_ts, or Webby_ts, or Chrono_ts
+####### xts: ts scheme from above, either: bts, wts, or cts
+
+######### here we define resolution of analysis
+if (selection == "b") {opx <- opb;
+                      x_ts <- "Bergst_ts";
+                      xts <- bts
+                      }
+if (selection == "c") {opx <- opc;
+                      x_ts <- "Chrono_ts";
+                      xts <- cts
+                      }
+if (selection == "w") {opx <- opw;
+                      x_ts <- "Webby_ts";
+                      xts <- wts
+                      }
+
+######### here we combine PBDB data with xts
+#########
+sn.1 <- merge(x=sn.r, y=n.r, by.x = "name_id", by.y = "id", all.x=TRUE)
+sn.1 <- merge(x=sn.1, y=q.r, by.x = "qualifier_id", by.y = "id", all.x=TRUE)
+sn <- sn.1[,c(3, 11, 18, 19)]
+pbdb.sn <-  merge(x=pb.u, y=sn, by.x = "structured_name_id", by.y = "id", all.x=TRUE)
+op.f <-  merge(x=pbdb.sn, y=opx, by.x = "name", by.y = "name", all.x=TRUE)
+
+######### here we create subset matches for each pbdb_qualifier
+
+ea <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="early_interval"), by.x = "early_interval", by.y = "pbdb_name", all.x=TRUE)
+ea <- ea[with(ea, order(ea$id.x)), ]
+
+la <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="late_interval"), by.x = "late_interval", by.y = "pbdb_name", all.x=TRUE)
+la <- la[with(la, order(la$id.x)), ]
+
+sa <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="stratgroup"), by.x = "stratgroup", by.y = "pbdb_name", all.x=TRUE)
+sa <- sa[with(sa, order(sa$id.x)), ]
+
+fa <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="formation"), by.x = "formation", by.y = "pbdb_name", all.x=TRUE)
+fa <- fa[with(fa, order(fa$id.x)), ]
+
+ma <- merge(x=os.c, y=unique(subset(op.f, op.f$pbdb_qualifier=="member")), by.x = "member", by.y = "pbdb_name", all.x=TRUE)
+ma <- ma[with(ma, order(ma$id.x)), ]
+
+za <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="zone"), by.x = "zone", by.y = "pbdb_name", all.x=TRUE)
+za <- za[with(za, order(za$id.x)), ]
+
+lla <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="localbed"), by.x = "localbed", by.y = "pbdb_name", all.x=TRUE)
+lla <- lla[with(lla, order(lla$id.x)), ]
+
+ra <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="regionalbed"), by.x = "regionalbed", by.y = "pbdb_name", all.x=TRUE)
+ra <- ra[with(ra, order(ra$id.x)), ]
+
+sta <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="stratcomments"), by.x = "stratcomments", by.y = "pbdb_name", all.x=TRUE)
+sta <- sta[with(sta, order(sta$id.x)), ]
+sta <- sta[!(duplicated(sta[c("id.x","stratcomments")]) | duplicated(sta[c("id.x","stratcomments")])), ]
+#xl <- as.data.frame(duplicated(sta$id.x))
+
+fma <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="fmm"), by.x = "fmm", by.y = "pbdb_name", all.x=TRUE)
+fma <- fma[with(fma, order(fma$id.x)), ]
+
+fba <- merge(x=os.c, y=subset(op.f, op.f$pbdb_qualifier=="flb"), by.x = "flb", by.y = "pbdb_name", all.x=TRUE)
+fba <- fba[with(fba, order(fba$id.x)), ]
+
+eat <- merge(x=ea, y=xts, by.x = "oldest", by.y = x_ts, all.x=TRUE)
+eat <- eat[with(eat, order(eat$id.x)), ]
+colnames(eat)[18] <- "reference"
+colnames(eat)[19] <- "ts_number"
+
+lat <- merge(x=la, y=xts, by.x = "youngest", by.y = x_ts, all.x=TRUE)
+lat <- lat[with(lat, order(lat$id.x)), ]
+colnames(lat)[19] <- "reference"
+colnames(lat)[19] <- "ts_number"
+
+########################
+######### we find shortest interval within different PBDB options for each occurrence and combine this with ordo_res this takes several hours to calculate
+
+lelo <- nrow(ra)
+ordo_res <- array()
+for (i in 1:nrow(ra)) {
+  iresult <- matrix(0,1,5, dimnames=list(c(),c("oldest", "youngest", "ts_count", "rules", "refs")))
+  
+  ## cont.a collects interval length in different resolutions
+  cont.a <- cbind((as.numeric(as.character(lat$ts_number[i]))-as.numeric(as.character(eat$ts_number[i]))), 
+                  as.numeric(as.character(ea$ts_count[i])),
+                  as.numeric(as.character(la$ts_count[i])),
+                  as.numeric(as.character(sa$ts_count[i])), 
+                  as.numeric(as.character(fa$ts_count[i])), 
+                  as.numeric(as.character(ma$ts_count[i])), 
+                  as.numeric(as.character(za$ts_count[i])), 
+                  as.numeric(as.character(lla$ts_count[i])),
+                  as.numeric(as.character(sta$ts_count[i])),
+                  as.numeric(as.character(fma$ts_count[i])),
+                  as.numeric(as.character(fba$ts_count[i])),
+                  as.numeric(as.character(ra$ts_count[i])))
+  cont.a[is.na(cont.a)] <- 1111
+  
+  ## interval, eai, lai, etc. concentenate all necassary fields for iresult
+  interval <- as.data.frame(c(as.character(eat$oldest[i]), as.character(lat$youngest[i]), cont.a[1], as.character(lat$rule_id[i]), as.character(lat$reference[i])))
+  eai <- as.data.frame(c(as.character(ea$oldest[i]), as.character(ea$youngest[i]), cont.a[2], as.character(ea$rule_id[i]), as.character(ea$reference[i])))
+  lai <- as.data.frame(c(as.character(la$oldest[i]), as.character(la$youngest[i]), cont.a[3], as.character(la$rule_id[i]), as.character(la$reference[i])))
+  sai <- as.data.frame(c(as.character(sa$oldest[i]), as.character(sa$youngest[i]), cont.a[4], as.character(sa$rule_id[i]), as.character(sa$reference[i])))
+  fai<- as.data.frame(c(as.character(fa$oldest[i]), as.character(fa$youngest[i]), cont.a[5], as.character(fa$rule_id[i]), as.character(fa$reference[i])))
+  mai<- as.data.frame(c(as.character(ma$oldest[i]), as.character(ma$youngest[i]), cont.a[6], as.character(ma$rule_id[i]), as.character(ma$reference[i])))
+  zai<- as.data.frame(c(as.character(za$oldest[i]), as.character(za$youngest[i]), cont.a[7], as.character(za$rule_id[i]), as.character(za$reference[i])))
+  llai<- as.data.frame(c(as.character(lla$oldest[i]), as.character(lla$youngest[i]), cont.a[8], as.character(lla$rule_id[i]), as.character(lla$reference[i])))
+  stai<- as.data.frame(c(as.character(sta$oldest[i]), as.character(sta$youngest[i]), cont.a[9], as.character(sta$rule_id[i]), as.character(sta$reference[i])))
+  fmai<- as.data.frame(c(as.character(fma$oldest[i]), as.character(fma$youngest[i]), cont.a[10], as.character(fma$rule_id[i]), as.character(fma$reference[i])))
+  fbai<- as.data.frame(c(as.character(fba$oldest[i]), as.character(fba$youngest[i]), cont.a[11], as.character(fba$rule_id[i]), as.character(fba$reference[i])))
+  rai<- as.data.frame(c(as.character(ra$oldest[i]), as.character(ra$youngest[i]), cont.a[12], as.character(ra$rule_id[i]), as.character(ra$reference[i])))
+  
+  ## cont.b combines all necessary fields from each resolution
+  cont.b <- cbind(interval, eai, lai, sai, fai, mai, zai, llai, stai, fmai, fbai, rai)
+  cont.b <- t(cont.b)
+  
+  ## we select either one interval or interval range
+  if(cont.b[3,3]=="1111"){cont.b<- cont.b[-3,]} else {cont.b<- cont.b[c(-1,-2),]}
+  
+  ## we select the highest resolution of the occurrence
+  initi <- unique(subset(cont.b,cont.b[,3]== min(as.numeric(as.character(cont.b[,3])))))
+  colnames(initi) <- c("a", "b", "c", "d", "e")
+  
+  ## in case there are more than one equally long resolutions we select the most direct correlation (lowest rule)
+  if(nrow(initi)>1){
+    initi <- subset(initi,initi[,4] == min(as.numeric(as.character(initi[,4]))))
+  } 
+  
+  ## in case there are more than one equally long most direct correlation (lowest rule), we select with the best theoretical solution
+  if(nrow(initi)>1){
+    initi <- initi[max(nrow(initi)),]
+  } 
+  
+  ## in case of complete nonmatch we create iresult with NAs
+  if(unique(cont.b[,3])=="1111"&&length(unique(cont.b[,3]))==1){iresult[1,]<- c("NA", "NA", "NA", "NA", "NA")}
+  
+  iresult[1,1] <- as.character(initi[1])
+  iresult[1,2] <- as.character(initi[2])
+  iresult[1,3] <- as.character(initi[3])
+  iresult[1,4] <- as.character(initi[4])
+  iresult[1,5] <- as.character(initi[5])
+  
+  #rbind for each i
+  ordo.line <- cbind(ordo_tot[i,], iresult)
+  ordo_res <- rbind(ordo_res, ordo.line)
+}
+ordo_res <- ordo_res[-1,]
+
+################# write out result
+
+if (selection == "b") {write.csv(ordo_res, file = "ordo_match_bergst.csv")}
+if (selection == "c") {write.csv(ordo_res, file = "ordo_match_chrono.csv")}
+if (selection == "w") {write.csv(ordo_res, file = "ordo_match_webby.csv")}
---
+++ rname_functions_cleanedx.R
@@ -0,0 +1,200 @@
+### functions for rnames
+
+library(DBI)
+library(RMySQL)
+library(gdata)
+library(splitstackshape)
+
+##### Variable for rnames
+m.drv <- dbDriver("MySQL")
+db.url <- "mysql.luomus.fi"
+db.port <- "3306"  
+db.user <- "xxx" ############ ---> insert you user name here
+db.pw <- "xxx" ############ ---> insert you user password here
+db.db <- "rnames"
+rn.con <- dbConnect(m.drv, dbname = db.db, username = db.user, password = db.pw, host = db.url)
+
+
+dbGetQuery(rn.con," SET NAMES utf8")
+	reftab <- dbGetQuery(rn.con,"select * from reference ")
+on.exit(dbDisconnect(rn.con))
+
+Bergst_ts <- c("Camb", "Tr1", "Tr2", "Tr3", "Fl1", "Fl2", "Fl3", "Dp1", "Dp2", "Dp3", "Dw1", "Dw2", "Dw3", "Sa1", "Sa2", "Ka1", "Ka2", "Ka3", "Ka4", "Hi1", "Hi2", "Sil")
+names(Bergst_ts) <- 1:22
+Bergst_ts <- as.data.frame(Bergst_ts)
+
+Webby_ts <- c("Camb","1a", "1b", "1c", "1d", "2a", "2b", "2c", "3a", "3b", "4a", "4b", "4c", "5a", "5b", "5c", "5d", "6a", "6b", "6c", "Sil")
+names(Webby_ts) <- 1:21
+Webby_ts <- as.data.frame(Webby_ts)
+
+Kroger_ts <- c("Camb", "Tr1", "Tr2", "Fl1", "Fl2", "Fl3", "Dp1", "Dp2", "Dw1", "Dw2", "Dw3", "Sa1", "Sa2", "Ka1", "Ka2", "Ka3", "Ka4", "Hi1", "Hi2", "Sil")
+names(Kroger_ts) <- 1:20
+Kroger_ts <- as.data.frame(Kroger_ts)
+
+Chrono_ts <- c("Cambrian", "Tremadocian", "Floian", "Dapingian", "Darriwilian", "Sandbian", "Katian", "Hirnantian", "Silurian")
+names(Chrono_ts) <- 1:9
+Chrono_ts <- as.data.frame(Chrono_ts)
+
+Bergst_abs <- c("497.0","485.4", "480.3", "480.0", "477.7", "473.7", "472.4", "470.0", "468.6", "468.2", "467.3", "465.3", "462.5", "458.4", "456.6", "453.0", "450.4", "448.9", "448.5", "445.2", "444.5", "443.8","440.8") # gives bases of StS, based on Cooper & Sadler (2012)
+names(Bergst_abs) <- 1:23
+Bergst_abs <- as.data.frame(Bergst_abs)
+b.diff <-  -as.numeric(as.character(Bergst_abs[-1,]))+as.numeric(as.character(Bergst_abs[1:nrow(Bergst_abs)-1,]))
+
+Chrono_abs <- c("497.0","485.4", "477.7", "470.0", "467.3", "458.4", "453.0", "445.2", "443.8","440.8") # gives bases of stages, based on Cooper & Sadler (2012)
+names(Chrono_abs) <- 1:10
+Chrono_abs <- as.data.frame(Chrono_abs)
+c.diff <-  -as.numeric(as.character(Chrono_abs[-1,]))+as.numeric(as.character(Chrono_abs[1:nrow(Chrono_abs)-1,]))
+
+Webby_abs <- c("497.0","485.4", "480.3", "480.0", "479.0", "477.7", "476.0", "473.4", "470.0", "468.7", "467.3", "465.3", "462.5", "458.4", "456.7", "453.8", "450.3", "448.4", "447.0", "445.2", "443.8", "440.8")# based on Cooper & Sadler (2012)
+names(Webby_abs) <- 1:22
+Webby_abs <- as.data.frame(Webby_abs)
+w.diff <-  -as.numeric(as.character(Webby_abs[-1,]))+as.numeric(as.character(Webby_abs[1:nrow(Webby_abs)-1,]))
+
+
+########################################		
+########################################
+######## This function is used in CREATE_script. It selects timeslices out of name_table based on compromise algorithm
+
+compromise.selector <- function (ts_used, name_table_to_ts) {
+  
+	if (ts_used == "B") {nts <- Bergst_ts}
+	if (ts_used == "W") {nts <- Webby_ts}
+	if (ts_used == "K") {nts <- Kroger_ts}
+  if (ts_used == "C") {nts <- Chrono_ts};
+	
+bios <- name_table_to_ts
+ebios <- cSplit(bios, "name_2", ",", direction = "long");
+ele <- unique(bios$name_1)
+lele <- length(ele)
+bio.sel <- array(, dim=c(0,5))
+colnames(bio.sel) <- c("name", "oldest", "youngest", "tscount", "refs");
+		
+for (i in 1:lele) {
+		bio_set <- subset(ebios, ebios$name_1 == ele[i])
+		i.n <- as.character(ele[i])
+		u.ref <- unique(bio_set$reference_id)
+		rle <- length(u.ref)
+		nle <- nrow(nts)
+    counts.per_ts <- array(, dim=c(0,5)) 
+		colnames(counts.per_ts) <- c("oldest", "youngest", "refs", "refcount", "tscount")
+			for (l in 1:nle) {				
+				for (m in l:nle) { 
+	 							name.c <-vector();
+								refnamesa <- subset(bio_set, as.character(bio_set$name_2) == as.character(nts[m,]));
+								name.c <-refnamesa$reference_id;
+	 							counts.per_ts <- rbind (counts.per_ts, c(l, m, paste(as.character(unique(name.c)), collapse=", ") , length(unique(name.c)), m-l+1)); ## builds array with interval max/min (=l, m), list of containing refs, length of that lisd, and length of interval; check with any number for l m
+								}	
+							}
+			######## this is selection core #############	
+		
+			cpts <- data.frame(counts.per_ts)
+			cpts.refmax <- subset(cpts, as.numeric(levels(cpts$refcount))[cpts$refcount] == max(as.numeric(levels(cpts$refcount))[cpts$refcount])) ## selects interval with max nr of references per name
+			cpts.tsmin <- subset(cpts.refmax, as.numeric(levels(cpts.refmax$tscount))[cpts.refmax$tscount] == min(as.numeric(levels(cpts.refmax$tscount))[cpts.refmax$tscount])) ## selects interval with min nr of time bins
+			
+			## this builds the output array #############
+			i.on <- min(as.numeric(levels(cpts.tsmin$oldest))[cpts.tsmin$oldest])
+			i.yn <- max(as.numeric(levels(cpts.tsmin$youngest))[cpts.tsmin$youngest])
+			i.r <- cSplit(cpts.tsmin, "refs", ",", direction = "long")
+			i.r <- paste(as.character(unique(i.r$refs)), collapse=", ") #??
+			i.c <- as.character(levels(cpts.tsmin$refcount))[cpts.tsmin$refcount]
+			i.c <- i.c[1]
+			i.o <- nts[i.on,1]
+			i.o <- as.character(levels(i.o))[i.o]
+			i.y <- nts[i.yn,1]
+			i.y <- as.character(levels(i.y))[i.y]
+			bio.sel <- rbind(bio.sel,c(i.n, i.o, i.y, i.yn-i.on+1, i.r))			
+		}
+		return(bio.sel)	
+		}
+
+########################################
+########################################
+######## This function is used in CREATE_script. It selects timeslices out of name_table based on youngest algorithm
+
+youngest.selector <- function (ts_used, name_table_to_ts) {
+  if (ts_used == "B") {nts <- Bergst_ts}
+  if (ts_used == "W") {nts <- Webby_ts}
+  if (ts_used == "K") {nts <- Kroger_ts}
+  if (ts_used == "C") {nts <- Chrono_ts}
+  
+  bios <- name_table_to_ts
+  ebios <- cSplit(bios, "name_2", ",", direction = "long");
+  
+  ele <- unique(bios$name_1)
+  lele <- length(ele)
+  
+  bio.sel <- array(, dim=c(0,5));
+  colnames(bio.sel) <- c("name", "oldest", "youngest", "tscount", "refs");
+  
+  for (i in 1:lele) {
+    bio_set <- subset(ebios, ebios$name_1 == ele[i]) # test use 6, or 4, or 68
+    i.n <- as.character(ele[i])
+    i.d <- as.vector(strsplit(as.character(bio_set$reference_id), " "))
+    i.d <- as.data.table(i.d)
+    i.d <- as.character(i.d[1,])
+    bio_set <-cbind(i.d, bio_set)
+    u.ref <- unique(bio_set$i.d)
+    refu <- reftab[reftab$id %in% u.ref,] ### the next three lines select youngest refs
+    ####
+    refy <- subset(refu, refu$year == max(refu$year))
+    bio_set<- bio_set[bio_set$i.d %in% refy$id,]
+    rle <- length(refy)
+    nle <- nrow(nts)
+    counts.per_ts <- array(, dim=c(0,5)) 
+    colnames(counts.per_ts) <- c("oldest", "youngest", "refs", "refcount", "tscount")
+    for (l in 1:nle) {					# l starting value of interval
+      for (m in l:nle) { 				# m starting value of interval, runs through ts
+        name.c <-vector();
+        refnamesa <- subset(bio_set, as.character(bio_set$name_2) == as.character(nts[m,])); #test use 17
+        name.c <-refnamesa$reference_id; # hier nur die refnames
+        counts.per_ts <- rbind (counts.per_ts, c(l, m, paste(as.character(unique(name.c)), collapse=", ") , length(unique(name.c)), m-l+1)); ## builds array with interval max/min (=l, m), list of containing refs, length of that lisd, and length of interval; check with any number for l m
+      }	
+    }	
+    ## this is selection core #############	
+    cpts <- data.frame(counts.per_ts)
+    cpts.refmax <- subset(cpts, as.numeric(levels(cpts$refcount))[cpts$refcount] == max(as.numeric(levels(cpts$refcount))[cpts$refcount])) ## selects interval with max nr of references / formations
+    cpts.tsmin <- subset(cpts.refmax, as.numeric(levels(cpts.refmax$tscount))[cpts.refmax$tscount] == min(as.numeric(levels(cpts.refmax$tscount))[cpts.refmax$tscount])) ## selects interval with min nr of ts
+    
+    ## this builds the output array #############
+    i.on <- min(as.numeric(levels(cpts.tsmin$oldest))[cpts.tsmin$oldest])
+    i.yn <- max(as.numeric(levels(cpts.tsmin$youngest))[cpts.tsmin$youngest])
+    i.r <- cSplit(cpts.tsmin, "refs", ",", direction = "long")
+    i.r <- paste(as.character(unique(i.r$refs)), collapse=", ")
+    i.c <- as.character(levels(cpts.tsmin$refcount))[cpts.tsmin$refcount]
+    i.c <- i.c[1]
+    i.o <- nts[i.on,1]
+    i.o <- as.character(levels(i.o))[i.o]
+    i.y <- nts[i.yn,1]
+    i.y <- as.character(levels(i.y))[i.y]
+    bio.sel <- rbind(bio.sel,c(i.n, i.o, i.y, i.yn-i.on+1, i.r))			
+  }
+  return(bio.sel)	
+}
+
+
+########################################
+#######################################
+###function for combining two temp_opinion data.frames in CREATE scripts
+combi.temp <- function(tempa,tempb){
+  com <- merge (tempa, tempa, by.x = "name", by.y = "name")
+  cax <- subset(com, ts_count.x < ts_count.y, select = c("name", "id.x", "date_time.x", "rule_id.x", "oldest.x", "youngest.x", "ts_count.x", "reference.x")); ## these are the shorter
+  cay <- subset(com, ts_count.x > ts_count.y, select = c("name", "id.y", "date_time.y", "rule_id.y", "oldest.y", "youngest.y", "ts_count.y", "reference.y")); ## these are the shorter indirectly linked names 
+  cas <- subset(com, ts_count.x == ts_count.y, select = c("name", "id.x", "date_time.y", "rule_id.y", "oldest.y", "youngest.y", "ts_count.y", "reference.x", "reference.y")); ## these are the names with identical length
+  cxa <- merge (cas, cax, by.x = "name", by.y = "name"); # just a comparison
+  cya <- merge (cas, cay, by.x = "name", by.y = "name"); # just a comparison
+  cs <- cbind(cas[,1:7], paste(cas$reference.x, cas$reference.y, sep= ", "))
+  in.b <- subset(tempb, !tempb$name %in% com$name, select = c("name", "id", "date_time", "rule_id", "oldest", "youngest", "ts_count", "reference")); ## the ones in b that are not overlapping
+  in.a <- subset(tempa, !tempa$name %in% com$name, select = c("name", "id", "date_time", "rule_id", "oldest", "youngest", "ts_count", "reference")); ## the ones in a that are not overlapping
+  combi.names <- rbind(in.a, in.b, setNames(cax, names(in.a)), setNames(cay, names(in.a)), setNames(cs, names(in.a)))
+  return(combi.names)
+}
+
+combib.temp <- function(tempa,tempb){
+  com <- merge (tempa, tempa, by.x = "name", by.y = "name")
+  cas <- subset(com, ts_count.x == ts_count.y, select = c("name", "id.x", "date_time.y", "rule_id.y", "oldest.y", "youngest.y", "ts_count.y", "reference.x", "reference.y")); ## these are the names with identical length
+  cs <- cbind(cas[,1:7], paste(cas$reference.x, cas$reference.y, sep= ", "))
+  in.b <- subset(tempb, !tempb$name %in% tempa$name, select = c("name", "id", "date_time", "rule_id", "oldest", "youngest", "ts_count", "reference")); ## the ones in b that are not in a
+  in.a <- subset(tempa, !tempa$name %in% cas$name, select = c("name", "id", "date_time", "rule_id", "oldest", "youngest", "ts_count", "reference")); ## the ones in a that are not overlapping
+  combi.names <- rbind(in.a, in.b, setNames(cs, names(in.a)))
+  return(combi.names)
+}
